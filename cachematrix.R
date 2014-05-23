a0f3315213d3da414d6819d41c9eff93e700b642
gbeyderman <gbeyderman@gmail.com>
2014-05-23 15:38:05 -0400

    initial (trial) commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create and cache the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
